eee1d6ebdd759ca1eec7896b73fc79513eca8a32
S1nt4x16 <50811498+S1nt4x16@users.noreply.github.com>
2019-08-04 22:47:00 +0700

    Add files via upload

---
+++ synt4x16.sh
@@ -0,0 +1,86 @@
+clear
+python2 coded.py
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo $red
+cat img.txt
+echo ""
+echo "\033[34;1m[$]\033[36;1m==================================\033[34;1m[$]"
+echo "\033[32;1mAuthor : SYNT4X16"
+echo "\033[37;1mWeb:https://tutorialtermuxsynt4x16.wordpress.com "
+echo "\033[35;1mversion Tools: 1"
+echo "\033[35;1mTeam : Single Attackers"
+echo "\033[33;1mKontak Me :  087788405277"
+echo "\033[31;1mPlease For Visit My Website:"
+echo "\033[92mSingle Attackers"
+echo "\033[36;1mPm Kalo Gak Ngerti 087788405277"
+echo "\033[34;1m[$]\033[36;1m==================================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[==============================================]>"
+echo $green "1.> Tools Spam SYNT4X16"
+echo "[==============================================]>"
+echo $purple "2.> Tools Lengkap SYNT4X16"
+echo "[==============================================]>"
+echo $cyan "3.> Tools Crack SYNT4X16"
+echo "[==============================================]>"
+echo $red "4.> Tools Buat Virus SYNT4X16"
+echo "[==============================================]>"
+echo $cyan "5.> Exit"
+echo "[==============================================]>"
+read -p "root@TOOLS-VALEN~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f mono12 -F gay "SYNT4X16"
+pkg install git
+git clone https://github.com/S1nt4x16/Sagiri-Kawaii
+cd Sagiri-Kawaii
+sh SPAMCALLv16.sh
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f mono12 -F gay "SYNT4X16"
+pkg install git
+git clone https://github.com/S1nt4x16/WibuTheBest
+cd WibuTheBest
+sh Tools-VR.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+toilet -f mono12 -F gay "SYNT4X16"
+pkg install git
+git clone https://github.com/S1nt4x16/WeAreNolep
+cd WeAreNolep
+sh crack.sh
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ] 
+then
+clear
+toilet -f mono12 -F gay "SYNT4X16"
+pkg install git
+git clone https://github.com/S1nt4x16/VirusCreater.git
+cd VirusCreater
+sh virusv16.sh
+echo "CARI DULU PASS.TXT TRS NANO PASS.TXT LIAT PW"
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f mono12 -F gay "SYNT4X16"
+exit
+fi
